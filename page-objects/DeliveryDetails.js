7572da1610afa4d0a42a254a14585cf2f8f6df0b
KotenkoNata <kotenkonata8@gmail.com>
2023-09-22 14:45:35 +0200

    feat: check values for inputs after fill in fields

--- page-objects/DeliveryDetails.js
+++ page-objects/DeliveryDetails.js
@@ -11,6 +11,12 @@ export class DeliveryDetails {
         this.countryDropDown = page.locator('[data-qa="country-dropdown"]');
         this.saveAddressButton = page.getByRole('button', { name: 'Save address for next time' });
         this.savedAddressContainer = page.locator('[data-qa="saved-address-container"]')
+        this.savedAddressLastName = page.locator('[data-qa="saved-address-lastName"]')
+        this.savedAddressFirstName = page.locator('[data-qa="saved-address-firstName"]')
+        this.savedAddressStreet = page.locator('[data-qa="saved-address-street"]')
+        this.savedAddressPostCode = page.locator('[data-qa="saved-address-postcode"]')
+        this.savedAddressCity = page.locator('[data-qa="saved-address-city"]');
+        this.savedAddressCountry = page.locator('[data-qa="saved-address-country"]');
     }
 
     fillDetails = async ({firstName,lastName, street, postCode, city, countryDropDown})=> {
@@ -38,5 +44,30 @@ export class DeliveryDetails {
         await this.saveAddressButton.waitFor();
         await this.saveAddressButton.click();
         await expect(this.savedAddressContainer).toHaveCount(addressCountBeforeSaving+1);
+
+        await this.savedAddressFirstName.first().waitFor();
+        expect(await this.savedAddressFirstName.first().innerText())
+            .toBe(await this.firstName.inputValue())
+
+        await this.savedAddressLastName.first().waitFor();
+        expect(await this.savedAddressLastName.first().innerText())
+            .toBe(await this.lastName.inputValue())
+
+        await this.savedAddressStreet.first().waitFor();
+        expect(await this.savedAddressStreet.first().innerText())
+            .toBe(await this.street.inputValue());
+
+        await this.savedAddressPostCode.first().waitFor();
+        expect(await this.savedAddressPostCode.first().innerText())
+            .toBe(await this.postCode.inputValue());
+
+        await this.savedAddressCity.first().waitFor();
+        expect(await this.savedAddressCity.first().innerText())
+            .toBe(await this.city.inputValue());
+
+        await this.savedAddressCountry.first().waitFor();
+        expect(await this.savedAddressCountry.first().innerText())
+            .toBe(await this.countryDropDown.inputValue())
+        await this.page.pause()
     }
 }
