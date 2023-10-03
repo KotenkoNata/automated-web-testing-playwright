1e66954c3805488480a433cf80f0b1d5dc9604d8
KotenkoNata <kotenkonata8@gmail.com>
2023-10-03 14:26:54 +0200

    feat: reusable API helpers

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,10 +1,10 @@
 import {test} from "@playwright/test";
 import {MyAccountPage} from "../page-objects/MyAccountPage";
-import {getLoginToken} from "./../api-calls/getLoginToken"
+import {getLoginToken} from "./../api-calls/getLoginToken";
+import {adminDetails} from "../data/userDetails";
 
 test.only("My account using cookie injection", async ({page})=>{
-    const loginToken = await getLoginToken();
-    console.warn({loginToken})
+    const loginToken = await getLoginToken(adminDetails.username, adminDetails.password);
 
     const myAccount = new MyAccountPage(page);
     await myAccount.visit();
