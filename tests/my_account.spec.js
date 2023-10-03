f83d2bd4b821791b22a2f22d60222905baf1bf4d
KotenkoNata <kotenkonata8@gmail.com>
2023-10-03 14:58:05 +0200

    feat: environment variables

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,6 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import {test} from "@playwright/test";
 import {MyAccountPage} from "../page-objects/MyAccountPage";
-import {getLoginToken} from "./../api-calls/getLoginToken";
+import {getLoginToken} from "../api-calls/getLoginToken";
 import {adminDetails} from "../data/userDetails";
 
 test.only("My account using cookie injection", async ({page})=>{
